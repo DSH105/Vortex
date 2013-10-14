c9c08b34c7df2facfdb1f5ff86bc407743cceeaa
DSH105 <jason.chirgwin@ozemail.com.au>
2013-10-14 21:34:58 +1100

    Project base

---
+++ src/main/java/io/github/dsh105/wind/Wind.java
@@ -0,0 +1,20 @@
+package io.github.dsh105.wind;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Project by DSH105
+ */
+
+public class Wind extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
