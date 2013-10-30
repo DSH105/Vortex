c8ea918ccb55d5fc169ca10f7ce72de3e9e21be5
DSH105 <jason.chirgwin@ozemail.com.au>
2013-10-30 21:08:20 +1100

    DBO policy compliant updater

--- src/main/java/io/github/dsh105/overcast/Overcast.java
+++ src/main/java/io/github/dsh105/overcast/Overcast.java
@@ -1,6 +1,11 @@
 package io.github.dsh105.overcast;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 /**
  * Project by DSH105
@@ -9,6 +14,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Overcast extends JavaPlugin {
 
     private static Overcast instance;
+
+    // Update data
+    public boolean update = false;
+    public String name = "";
+    public long size = 0;
+    public boolean updateChecked = false;
 
     @Override
     public void onEnable() {
@@ -22,5 +33,47 @@ public class Overcast extends JavaPlugin {
 
     public static Overcast getInstance() {
         return instance;
+    }
+
+    protected void checkUpdates() {
+        if (this.getConfig(ConfigType.MAIN).getBoolean("checkForUpdates", true)) {
+            final File file = this.getFile();
+            final Updater.UpdateType updateType = this.getConfig(ConfigType.MAIN).getBoolean("autoUpdate", false) ? Updater.UpdateType.DEFAULT.DEFAULT : Updater.UpdateType.NO_DOWNLOAD;
+            getServer().getScheduler().runTaskAsynchronously(this, new Runnable() {
+                @Override
+                public void run() {
+                    Updater updater = new Updater(plugin, 67541, file, updateType, false);
+                    update = updater.getResult() == Updater.UpdateResult.UPDATE_AVAILABLE;
+                    if (update) {
+                        name = updater.getLatestName();
+                        ConsoleLogger.log(ChatColor.GOLD + "An update is available: " + name);
+                        ConsoleLogger.log(ChatColor.GOLD + "Type /ocupdate to update.");
+                        if (!updateChecked) {
+                            updateChecked = true;
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (commandLabel.equalsIgnoreCase("ocupdate")) {
+            if (sender.hasPermission("overcast.update")) {
+                if (updateChecked) {
+                    @SuppressWarnings("unused")
+                    Updater updater = new Updater(this, 67541, this.getFile(), Updater.UpdateType.NO_VERSION_CHECK, true);
+                    return true;
+                } else {
+                    sender.sendMessage(this.prefix + primaryColour + " An update is not available.");
+                    return true;
+                }
+            } else {
+                Lang.sendTo(sender, Lang.NO_PERMISSION.toString().replace("%perm%", "overcast.update"));
+                return true;
+            }
+        }
+        return false;
     }
 }
